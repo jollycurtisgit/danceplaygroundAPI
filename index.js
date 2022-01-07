beec3cfed588e43ac00e0442f556dc5989c8b94b
JollyChua <jollygrace.c.3@gmail.com>
2022-01-07 17:03:17 +0000

    installed npm, express and added routes

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+let app = express();
+
+app.get('/hello', function(req, res) {
+    res.send("Hello World! Orayt!")
+})
+
+app.get('', function(req, res) {
+    res.send("Hello Jolly!")
+})
+
+app.listen(3000, function(){
+    console.log("server has started")
+})
