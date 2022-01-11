e4de83a178cd7f37d29e7bb8a34920b79c9cc10e
JollyChua <74226122+JollyChua@users.noreply.github.com>
2022-01-11 15:45:10 +0800

    added app.get to retrieve posted data

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-//MongoDB
+//------------MongoDB
 require("./models/db");
 
 const express = require("express");
 let app = express();
 
-//Routes
-require("./routes/JSroutes");
+//--------------Routes
+require("./routes/JSroutes.js");
 
-//Port
+//------------Pattern
+const classes = [
+  {
+    id: 1,
+    name: "Zumba",
+    location: "NorthWood Village",
+    price: "P50",
+    schedule: "every Monday, 3PM"
+  }
+];
+
+app.get("/classes", function (req, res) {
+  res.send(classes);
+});
+
+app.get("/classes/:id", function (req, res) {
+  let findAclass = classes.find((c) => c.id === parseInt(req.params.id));
+  if (!findAclass) res.status(404).send("The class with that id was not found");
+  res.send(findAclass);
+});
+
+app.get("", function (req, res) {
+  res.send("Hello Jolly!");
+});
+
+app.post;
+
+//---------------Port
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
