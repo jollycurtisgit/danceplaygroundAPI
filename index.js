71af9d2b9b9e6bde695f106e6e53e21777846b66
JollyChua <74226122+JollyChua@users.noreply.github.com>
2022-01-11 16:23:04 +0800

    created one post, but data wont post on postman, always timeout

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("./models/db");
 
 const express = require("express");
 let app = express();
+app.use(express.json());
 
 //--------------Routes
 require("./routes/JSroutes.js");
@@ -32,7 +33,17 @@ app.get("", function (req, res) {
   res.send("Hello Jolly!");
 });
 
-app.post;
+app.post("/classes", function (req, res) {
+    const class = {
+         id: classes.length + 1,
+         name: req.body.name,
+         location: req.body.location,
+         price: req.body.price,
+         schedule: req.body.schedule
+    };
+    classes.push(class);
+    res.send(class)
+})
 
 //---------------Port
 const port = process.env.PORT || 3000;
