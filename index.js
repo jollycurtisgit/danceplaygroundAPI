029a1bd542e9e7c20ba9d9258d5c24f11462c97e
JollyChua <74226122+JollyChua@users.noreply.github.com>
2022-01-11 14:55:38 +0800

    moved routes to JSroutes.JS

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
+//MongoDB
 require('./models/db');
 
 const express = require('express')
 let app = express();
 
 //Routes
+require('./routes/JSroutes');
 
-app.get('/hello', function(req, res) {
-    res.send("Hello World! Orayt!")
-})
-
-app.get('', function(req, res) {
-    res.send("Hello Jolly!")
-})
 
 app.listen(3000, function(){
     console.log("server has started")
