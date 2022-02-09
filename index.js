51b5bb6e7357d5e54a524e4ed32804602fb14848
jollycurtisgit <99135852+jollycurtisgit@users.noreply.github.com>
2022-02-09 21:35:09 +0800

    added functions for signUp

--- index.js
+++ index.js
@@ -78,13 +78,13 @@ function ensureToken(req, res, next){
 async function main(){
   await MongoUtil.connect(MONGO_URI, "danceplaygroundAPI");
 
-   // working - Display all added classes  
+   // 1. working - Display all added classes  
   app.get("/AddClasses", async function (req, res) {
     const db = MongoUtil.getDB();
     let classRecords = await db.collection('classes').find({}).toArray();
     res.send(classRecords);
   });
-  // working - Pull-up a class
+  // 2. working - Pull-up a class
   app.get("/class/:id", async function (req, res) {
     let id = req.params.id
     const db = MongoUtil.getDB();
@@ -96,8 +96,32 @@ async function main(){
     else{  
     res.send(findAclass)};
   });
+   
+  
+  //3. Wonderful! It's working! - Add a class
+  app.post("/TryClass", async function (req, res) {
+    console.log(req.body)
+    const postAclass = {
+      name: req.body.name,
+      location: req.body.location,
+      price: req.body.price,
+      schedule: req.body.schedule,
+      link: req.body.link,
+      email: req.body.email,
+      password: req.body.password,
+      duration: req.body.duration,
+      complexity: [req.body.complexity],
+      best_for: req.body.best_for,
+      category: req.body.category,
+      instructorName: req.body.instructorName,
+    };
+    const db = MongoUtil.getDB();
+    const classes = await db.collection('classes').insertOne(postAclass)
+    res.send(classes);
+  });
 
-  // working - Edit a class 
+
+  //4. working - Edit a class 
   app.patch("/class/:id", async function (req, res) {
     let id = req.params.id
     const db = MongoUtil.getDB()
@@ -123,7 +147,8 @@ async function main(){
     });
     res.send(updateAclass)
   });
-  //working - Delete a class 
+
+  // 5.working - Delete a class 
   app.delete("/delete.class/:id", async function(req, res){
     const db = MongoUtil.getDB()
     let deleteAclass = {
@@ -139,6 +164,38 @@ async function main(){
     res.send(deleteAclass)
   })
   
+ //6. working - Display all 30 mins classes  
+ app.get("/durationClassesSearch", async function (req, res) {
+  const db = MongoUtil.getDB();
+  let classRecords = await db.collection('classes').find(
+    {"duration":"30mins"}
+    ).toArray();
+  res.send(classRecords);
+});
+
+ //7. working - Display all 1hr classes  
+ app.get("/oneHR", async function (req, res) {
+  const db = MongoUtil.getDB();
+  let classRecords = await db.collection('classes').find(
+    {"duration":"1hr"}
+    ).toArray();
+  res.send(classRecords);
+});
+
+//8. working - Display all 2hr classes  
+app.get("/twoHR", async function (req, res) {
+  const db = MongoUtil.getDB();
+  let classRecords = await db.collection('classes').find(
+    {"duration":"2hrs"}
+    ).toArray();
+  res.send(classRecords);
+});
+
+
+//
+
+
+
   app.get('/delete.class.2/:id', async function(req, res){
     //retrieve from the mongo db the document with the same req.params.id
     const db = MongoUtil.getDB();
@@ -153,12 +210,6 @@ async function main(){
 
 
   //Routes for Login
-  // working - Display all added users
-//  app.get("", async function (req, res) {
-  //  const db = MongoUtil.getDB();
-   // let logInDetails = await db.collection('logIn').find({}).toArray();
-   // res.send(logInDetails);
-  //});
   // // working - Post a user
   // app.post("", async function (req, res) {
   //   const userLogIn = {
@@ -179,29 +230,6 @@ async function main(){
     let classRecords = await db.collection('classes').find({}).toArray();
     res.send(classRecords);
   });
-
-  //Wonderful! It's working! - Add a class
-  app.post("/TryClass", async function (req, res) {
-    console.log(req.body)
-    const postAclass = {
-      name: req.body.name,
-      location: req.body.location,
-      price: req.body.price,
-      schedule: req.body.schedule,
-      link: req.body.link,
-      email: req.body.email,
-      password: req.body.password,
-      duration: req.body.duration,
-      complexity: [req.body.complexity],
-      best_for: req.body.best_for,
-      category: req.body.category,
-      instructorName: req.body.instructorName,
-    };
-    const db = MongoUtil.getDB();
-    const classes = await db.collection('classes').insertOne(postAclass)
-    res.send(classes);
-  });
-
 
   // working - Edit a class  2
   app.patch("/Eclass/:id", async function (req, res) {
@@ -226,6 +254,13 @@ async function main(){
     }
   });
   //End of Trial Second Batch 2
+   // working - Display all added users
+ app.get("/SignUp", async function (req, res) {
+    const db = MongoUtil.getDB();
+    let logInDetails = await db.collection('logIn').find({}).toArray();
+    res.send(logInDetails);
+  });
+
 
   // working - Post a user
   app.post("", async function (req, res) {
@@ -254,6 +289,36 @@ async function main(){
     })
   });
 
+
+  //6. working - Display all 30 mins classes  
+ app.get("/durationClassesSearch", async function (req, res) {
+  const db = MongoUtil.getDB();
+  let classRecords = await db.collection('classes').find(
+    {"duration":"30mins"}
+    ).toArray();
+  res.send(classRecords);
+});
+    // Trial- Post a user
+   app.post("/SignUpTry", async function (req, res) {
+    const db = MongoUtil.getDB();
+    const postAmember = {
+     fName: req.body.fName,
+     lName: req.body.lName,
+     userName: req.body.userName,
+     email: req.body.email,
+     password: req.body.password,
+     password_confirm: req.body.password_confirm,
+     };
+    let emailRecords = await db.collection('SignUp').find(
+      {"email":req.body.email}
+      ).toArray();
+    if (emailRecords.length == 0){
+      await db.collection('SignUp').insertOne(postAmember)
+      res.send(postAmember);
+    }else{res.sendStatus(403)}  
+ });
+
+
    // working - Post a user
    app.post("/SignUp", async function (req, res) {
      const postAmember = {
@@ -263,7 +328,8 @@ async function main(){
       email: req.body.email,
       password: req.body.password,
       password_confirm: req.body.password_confirm,
-  };
+      };
+
      const db = MongoUtil.getDB();
      await db.collection('SignUp').insertOne(postAmember)
     res.send(postAmember);
